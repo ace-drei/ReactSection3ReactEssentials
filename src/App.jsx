aefbb74cd00ae448f62833cd97364b791b2c0673
Dre <aciobirca97@gmail.com>
2024-02-01 17:02:57 +0000

    fixed slight bug

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function Header() {
 function CoreConcept({image, title, description}) {
   return (
     <li>
-      <img src={image} alt={props.title} />
+      <img src={image} alt={title} />
       <h3> {title} </h3>
       <p> {description}</p>
     </li>
